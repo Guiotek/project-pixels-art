74c2eacdf5f15eddd7169320d757aebff85ff27e
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-07-05 20:03:41 -0300

    até o requisito 6

---
+++ script.js
@@ -0,0 +1,39 @@
+const colorBlack = document.body.getElementById('color-palette').getElementById('color1');
+const colorRed = document.body.getElementById('color-palette').getElementById('color2');
+const colorAqua = document.body.getElementById('color-palette').getElementById('color3');
+const colorOrange = document.body.getElementById('color-palette').getElementById('color4');
+const clickColor = document.body.getElementById('color-palette').querySelectorAll('.color')
+
+window.onload = colorBlack.classList.add('color selected');
+colorBlack.addEventListener("click",selectColor)
+function selectColor(){
+let Cor = "Black"
+    // if (<div class="color" id="color1"></div>) {
+    //     document.body.getElementById('color-palette').getElementById('color1').classlist.add("color selected")
+    //     document.body.getElementById('color-palette').getElementById('color2').className = "color"
+    //     document.body.getElementById('color-palette').getElementById('color3').className = "color"
+    //     document.body.getElementById('color-palette').getElementById('color4').className = "color"
+    // } else if (<div class="color" id="color2"></div>) {
+    //     document.body.getElementById('color-palette').getElementById('color1').className = "color"
+    //     document.body.getElementById('color-palette').getElementById('color2').className = "color selected"
+    //     document.body.getElementById('color-palette').getElementById('color3').className = "color"
+    //     document.body.getElementById('color-palette').getElementById('color4').className = "color"
+    //     Cor = "red"
+    // } else if (<div class="color" id="color3"></div>) {
+    //     document.body.getElementById('color-palette').getElementById('color1').className = "color"
+    //     document.body.getElementById('color-palette').getElementById('color2').className = "color"
+    //     document.body.getElementById('color-palette').getElementById('color3').className = "color selected"
+    //     document.body.getElementById('color-palette').getElementById('color4').className = "color"
+    //     Cor = "aqua"
+    // } else if (<div class="color" id="color4"></div>){
+    //     document.body.getElementById('color-palette').getElementById('color1').className = "color"
+    //     document.body.getElementById('color-palette').getElementById('color2').className = "color"
+    //     document.body.getElementById('color-palette').getElementById('color3').className = "color"
+    //     document.body.getElementById('color-palette').getElementById('color4').className = "color selected"
+    //     Cor = "orange"
+    // }
+    return Cor
+}
+console.log(selectColor);
+
+// cor clicada deve receber class="selected"
